4f290a2b8dce35bd725d1369deb8bbb176c2f436
Dhruvil Patel <dhruvilpatel2002@gmail.com>
2022-01-07 14:09:49 +0530

    added submit by enter key

--- public/client.js
+++ public/client.js
@@ -1,35 +1,36 @@
 const socket = io("http://localhost:3001");
 socket.on("connection");
 let messageArea = document.querySelector(".message__area");
-/* socket.on("message", (data) => {
-  document.querySelector("h3").innerHTML = data;
-});
-const sendMessage = () => {
-  const info = document.querySelector(".message").value;
-  socket.emit("message", info);
-}; */
+
 textarea.addEventListener("keyup", (e) => {
+  //to listen for enter key press
   if (e.key === "Enter") {
     sendMessage(e.target.value);
   }
 });
 socket.on("message", (data) => {
+  //listening for message from server
   appendMessage(data, "incoming"); //appending div to message area
-  scrollToBottom();
+  //scrollToBottom();
 });
 const sendMessage = () => {
+  //to send message to server
   const info = document.querySelector(".message").value;
   const v1 = document.getElementById("textarea");
   data = { info, username };
-  v1.value = "";
+  v1.value = ""; //clearing the textarea after sending message
   if (info.length > 0) {
-    appendMessage(data, "row justify-content-end height-fit-content");
-    scrollToBottom(); //appending div to message area
-    socket.emit("message", data);
+    socket.emit("message", data); //sending to the server before editing username to You as I wanted to display You in the users page
+    appendMessage(data, "row justify-content-end height-fit-content"); //appending data to message area
+    //scrollToBottom();
   }
 };
 
 function appendMessage(data, type) {
+  //to append message to message area
+  if (type !== "incoming") {
+    data.username = "You";
+  }
   let maindiv = document.createElement("div"); //creating new div
   maindiv.className = type;
   maindiv.innerHTML = `
@@ -41,4 +42,7 @@ function appendMessage(data, type) {
   messageArea.appendChild(maindiv); //appending div to message area
 }
 
-//function scrollToBottom() {}
+/* function scrollToBottom() {
+  var objDiv = document.getElementById(div);
+  objDiv.scrollTop = objDiv.scrollHeight;
+} */

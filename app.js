b430df3408c2f5cc33d4dc9460c1a5e70d12d45b
Dhruvil Patel <dhruvilpatel2002@gmail.com>
2022-01-12 00:30:03 +0530

    all users added

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const DBURI =
+  "mongodb+srv://dhruvil1808:Dhruvil1234@dhruvils-db.6wkxh.mongodb.net/ChatApp?retryWrites=true&w=majority";
+const appRoutes = require("./routes/appRoutes");
 var port = process.env.PORT || 3000;
 const app = express();
 const { Server } = require("socket.io");
@@ -8,11 +12,6 @@ const io = new Server(3001, {
     origin: "*",
   },
 });
-
-const mongoose = require("mongoose");
-const User = require("./model/credentials");
-const DBURI =
-  "mongodb+srv://dhruvil1808:Dhruvil1234@dhruvils-db.6wkxh.mongodb.net/ChatApp?retryWrites=true&w=majority";
 
 app.set("view engine", "ejs");
 mongoose
@@ -30,60 +29,5 @@ io.on("connection", (socket) => {
     socket.broadcast.emit("message", data);
   });
 });
-
-app.post("/create-user", (req, res) => {
-  const user = new User(req.body);
-  user
-    .save()
-    .then((result) => {
-      res.render("index", {
-        title: "ChatApp",
-        alrt: "User Created Successfully",
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-app.get("/", (req, res) => {
-  res.render("index", { title: "ChatApp", alrt: "" });
-});
-app.get("/about", (req, res) => {
-  res.render("about", { title: "About" });
-});
-/* app.get("/all-users", async (req, res) => {
-  const name = req.query.search1;
-  const pass = req.query.search2;
-  const result = await User.findOne({ username: name, password: pass });
-  if (result != null) {
-    User.find({})
-      .sort({ createdAt: -1 })
-      .then((results) => {
-        res.render("all-users", { users: results, title: result.username });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  } else {
-    res.render("index", { title: "ChatApp", alrt: "Invalid Credentials" });
-  }
-}); */
-app.get("/chat", async (req, res) => {
-  const name = req.query.search1;
-  const pass = req.query.search2;
-  const result = await User.findOne({ username: name, password: pass });
-  if (result != null) {
-    res.render("chat", {
-      user: result,
-      title: result.username,
-    });
-  } else {
-    res.render("index", { title: "ChatApp", alrt: "Invalid Credentials" });
-  }
-});
-app.get("/sign-up", (req, res) => {
-  res.render("sign-up", { title: "Sign Up" });
-});
-app.use((req, res) => {
-  res.render("404", { title: "404 Error" });
-});
+//routes
+app.use(appRoutes);
---
+++ routes/appRoutes.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const User = require("../model/credentials");
+const router = express.Router();
+
+router.post("/create-user", (req, res) => {
+  const user = new User(req.body);
+  user
+    .save()
+    .then((result) => {
+      res.render("index", {
+        title: "ChatApp",
+        alrt: "User Created Successfully",
+      });
+    })
+    .catch((err) => {
+      res.render("404", { title: "404 Error" });
+    });
+});
+router.get("/", (req, res) => {
+  res.render("index", { title: "ChatApp", alrt: "" });
+});
+router.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+router.get("/chat/:username/:select", async (req, res) => {
+  const select = req.params.select;
+  const username = req.params.username;
+  console.log(select, username);
+  res.render("chat", {
+    title: username,
+    select: select,
+  });
+});
+router.get("/sign-up", (req, res) => {
+  res.render("sign-up", { title: "Sign Up" });
+});
+router.get("/all-users", async (req, res) => {
+  const name = req.query.search1;
+  const pass = req.query.search2;
+  const result = await User.findOne({ username: name, password: pass });
+  if (result != null) {
+    User.find({})
+      .sort({ createdAt: -1 })
+      .then((results) => {
+        res.render("all-users", { users: results, title: result.username });
+      })
+      .catch((err) => {
+        res.render("404", { title: "404 Error" });
+      });
+  } else {
+    res.render("index", { title: "ChatApp", alrt: "Invalid Credentials" });
+  }
+});
+router.use((req, res) => {
+  res.render("404", { title: "404 Error" });
+});
+
+module.exports = router;
